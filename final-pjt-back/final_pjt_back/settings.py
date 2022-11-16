f2592176fd0a700cd69321da863630c018f69e35
권용재 <yongjae116@gmail.com>
2022-11-16 23:43:11 +0900

    1116_수정2

--- final-pjt-back/final_pjt_back/settings.py
+++ final-pjt-back/final_pjt_back/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'keyword_extractor',
+    'rest_framework',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
