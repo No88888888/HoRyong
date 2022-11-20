219b246a5de1adc01c737a1cb17ad6b69d25d41a
김진호 <wlsghsla94@naver.com>
2022-11-20 17:45:26 +0900

    로그인,싸인업 url 수정

--- final-pjt-back/accounts/views.py
+++ final-pjt-back/accounts/views.py
@@ -1,58 +1,58 @@
-from django.contrib.auth import login as auth_login
-from rest_framework.response import Response
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
-from django.views.decorators.http import require_http_methods, require_POST
-from .forms import CustomUserChangeForm, CustomUserCreationForm
-
+from django.shortcuts import render
+# from django.contrib.auth import login as auth_login
+# from rest_framework.response import Response
+# from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+# from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+# from django.views.decorators.http import require_http_methods, require_POST
+# from .forms import CustomUserChangeForm, CustomUserCreationForm
 # Create your views here.
 
-@require_http_methods(['GET', 'POST'])
-def login(request):
-    if request.user.is_authenticated:
-        return redirect('articles:index')
+# @require_http_methods(['GET', 'POST'])
+# def login(request):
+#     if request.user.is_authenticated:
+#         return redirect('articles:index')
 
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        # form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            # 로그인
-            auth_login(request, form.get_user())
-            return redirect(request.GET.get('next') or 'articles:index')
-    else:
-        form = AuthenticationForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'accounts/login.html', context)
+#     if request.method == 'POST':
+#         form = AuthenticationForm(request, request.POST)
+#         # form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             # 로그인
+#             auth_login(request, form.get_user())
+#             return redirect(request.GET.get('next') or 'articles:index')
+#     else:
+#         form = AuthenticationForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'accounts/login.html', context)
     
-def logout(request):
-    pass
+# def logout(request):
+#     pass
     
-@require_http_methods(['GET', 'POST'])
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('articles:index')
+# @require_http_methods(['GET', 'POST'])
+# def signup(request):
+#     if request.user.is_authenticated:
+#         return redirect('articles:index')
         
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            # 회원가입 후 로그인
-            auth_login(request, user)
-            return redirect('articles:index')
-    else:
-        form = CustomUserCreationForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'accounts/signup.html', context)
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             # 회원가입 후 로그인
+#             auth_login(request, user)
+#             return redirect('articles:index')
+#     else:
+#         form = CustomUserCreationForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'accounts/signup.html', context)
     
 
 
                 
                 
                 
-def wish_list(request):
-    pass
+# def wish_list(request):
+#     pass
     
--- final-pjt-back/final_pjt_back/urls.py
+++ final-pjt-back/final_pjt_back/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('keyword_extractor/', include('keyword_extractor.urls')),
     path('movies/', include('movies.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('dj_rest_auth.urls')),
+    path('accounts/signup/', include('dj_rest_auth.registration.urls'))
 ]
 
